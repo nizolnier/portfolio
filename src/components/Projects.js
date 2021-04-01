213ff76509354fff8b784bda6ecd447c38d5c449
Nicole <nzolnier@gmail.com>
2021-04-01 18:56:37 -0300

    add components and project array

---
+++ src/components/Projects.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+const Projects = () => {
+  const projects = [{
+    title: "Astromatch - Frontend",
+    description: "",
+    github: "https://github.com/nizolnier/astromatch",
+    website: "",
+    image: ""
+  },{
+    title: "Cookenu - Backend",
+    description: "",
+    github: "https://github.com/nizolnier/cookenu",
+    website: "",
+    image: ""
+  },
+  {
+    title: "Greenddit - Frontend",
+    description: "",
+    github: "https://github.com/nizolnier/cookenu",
+    website: "",
+    image: ""
+  },
+  {
+    title: "Labegram - Backend",
+    description: "",
+    github: "https://github.com/nizolnier/projeto-fullstack-backend",
+    website: "",
+    image: ""
+  },
+  {
+    title: "Labegram - Frontend",
+    description: "",
+    github: "https://github.com/nizolnier/projeto-fullstack-frontend",
+    website: "",
+    image: ""
+  },
+  {
+    title: "LAMA Backend",
+    description: "",
+    github: "https://github.com/nizolnier/projeto-fullstack-backend",
+    website: "",
+    image: ""
+  },
+  {
+    title: "StarkX",
+    description: "",
+    github: "https://github.com/nizolnier/starkX",
+    website: "",
+    image: ""
+  }]
+
+  return (
+    <div>Projects</div>
+  )
+}
+
+export default Projects;
