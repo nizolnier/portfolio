3e9726add0de31cc90c8b2e5624ad0a1da08803a
Nicole <nzolnier@gmail.com>
2021-04-02 11:45:31 -0300

    add header, footer, medias

---
+++ src/components/Footer/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const FooterContainer = styled.div`
+    width: 100%;
+    height: 8vh;
+    background-color: black;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+`
+
+export const Credits = styled.p`
+    color: white;
+    padding-left: 2em;
+`
