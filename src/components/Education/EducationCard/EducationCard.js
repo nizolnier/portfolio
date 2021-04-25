da3b185b755e893bbca0faec815917e038ec3711
Nicole <nzolnier@gmail.com>
2021-04-25 18:25:20 -0300

    add finished education component

--- src/components/Education/EducationCard/EducationCard.js
+++ src/components/Education/EducationCard/EducationCard.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Rectangle } from './styles'
-// import { Heading, Text } from "@chakra-ui/react"
+import { Heading, Text } from "@chakra-ui/react"
 
 const EducationCard = ({title, institution, year, description}) => {
   return (
     <Rectangle>
-      <h1>{title}</h1>
-      <p>{institution} - {year}</p>
-      <p>{description}</p>
+      <Heading textAlign="left" as="h3" size="md">{title} - {institution}</Heading>
+      <Text fontSize="xs" pb="0.4em" pt="0.4em">{year}</Text>
+      <Text fontSize="md">{description}</Text>
     </Rectangle>
   )
 }
